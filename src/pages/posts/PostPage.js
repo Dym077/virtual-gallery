8b7b4f504d0bf0b211493ffc7761ebe062e5219f
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-06-20 12:31:45 +0000

    Correct errors in code

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import { axiosReq } from "../../api/axiosDefaults";
 
 import appStyles from "../../App.module.css";
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router";
 
 function PostPage() {
   const { id } = useParams();
