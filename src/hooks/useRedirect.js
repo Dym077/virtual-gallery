d1cd63a3808174f003cf716c18b48e8ec23f5386
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-06-25 12:12:58 +0000

    Add code to redirect user to hompage

---
+++ src/hooks/useRedirect.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useHistory } from "react-router";
+
+export const useRedirect = (userAuthStatus) => {
+  const history = useHistory();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        await axios.post("/dj-rest-auth/token/refresh/");
+        // if the user is logged in, this code will run
+        if (userAuthStatus === "loggedin") {
+          history.push("/");
+        }
+      } catch (err) {
+        // if the user is not logged in, the code below will run instead
+        if (userAuthStatus === "loggedOut") {
+          history.push("/");
+        }
+      }
+    };
+    handleMount();
+  }, [history, userAuthStatus]);
+};
