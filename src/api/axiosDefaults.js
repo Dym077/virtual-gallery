0012e55accc008544e788151265d15dfa9b77052
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-07-15 12:06:34 +0000

    Revert to prod settings

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://drf-api1vg-e56998f2fbc1.herokuapp.com/";
-axios.defaults.baseURL = "https://drf-api1vg-e56998f2fbc1.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
